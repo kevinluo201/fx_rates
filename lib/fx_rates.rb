6dd5b223802e652ee2d61f3211430936abdb9b08
Kevin Luo <kevin.luo@telus.com>
2025-02-12 21:44:50 -0500

    init

---
+++ lib/fx_rates.rb
@@ -0,0 +1,6 @@
+require "fx_rates/version"
+
+module FxRates
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fx_rates_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe FxRates do
+  it "has a version number" do
+    expect(FxRates::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
