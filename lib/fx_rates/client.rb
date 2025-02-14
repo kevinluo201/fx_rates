593f585452a2428bc4a7d453eee921923735f3bc
Kevin Luo <kevin.luo@telus.com>
2025-02-13 21:49:36 -0500

    add historical

--- lib/fx_rates/client.rb
+++ lib/fx_rates/client.rb
@@ -11,15 +11,30 @@ module FXRates
       query[:amount] = params[:amount] || 1
       query[:places] = params[:places] if params[:places]
       query[:resolution] = params[:resolution] if params[:resolution]
-      if params[:currencies] && params[:currencies].is_a?(Array)
-        query[:currencies] = params[:currencies].join(',')
-      end
+      query[:currencies] = currencies_params(params[:currencies]) if params[:currencies]
 
       RatesResponse.new(@requset.get('/latest', query))
     end
 
+    def historical(**params)
+      query = {}
+      query[:date] = (params[:date] || Time.now.utc).strftime('%Y-%m-%d')
+      query[:base] = params[:base] || 'USD'
+      query[:currencies] = currencies_params(params[:currencies]) if params[:currencies]
+      query[:amount] = params[:amount] || 1
+      query[:places] = params[:places] if params[:places]
+
+      RatesResponse.new(@requset.get('/historical', query))
+    end
+
     def currencies
       @requset.get('/currencies')
+    end
+
+    private
+
+    def currencies_params(currencies)
+      currencies.is_a?(Array) ? currencies.join(',') : nil
     end
   end
 end
